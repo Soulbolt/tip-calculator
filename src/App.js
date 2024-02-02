b6f252ccc1b87477bfaf8e7ea3e095ca28c4dd9d
Soulbolt <soulbolt@gmail.com>
2024-02-02 11:16:54 -0700

    Add base components: Display label for bill  and percentage and their inputs fields.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TipCalculator />
+    </div>
+  );
 }
+
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage />
+      <SelectPercentage />
+      <OutPut />
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput() {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input type="text" placeholder="Bill value" />
+    </div>
+  );
+}
+
+function SelectPercentage() {
+  return (
+    <div>
+      What percentage would you tip?
+      <input type="text" />
+    </div>
+  );
+}
+
+function OutPut() {}
+
+function Reset() {}
